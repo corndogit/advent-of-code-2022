b88c8118f0539e4063861eaead82a31c93ddb297
corndogit <nathanfinlaybrenton@gmail.com>
2022-12-01 15:06:32 +0000

    Golfed down to 2 lines

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,27 +1,23 @@
 # Golfed solution:
-with open("../inputs/day1.txt") as file:
-    meals = list(map(lambda x: list(map(lambda y: int(y if len(y) > 0 else 0), x.split('\n'))),
-                     file.read().split('\n\n')))
-    print(f"Part 1: {max(map(sum, [m for m in meals]))}",
-          f"Part 2: {sum(sorted(map(sum, [m for m in meals]), reverse=True)[:3])}")
-
+with open("../inputs/day1.txt") as file: meals = list(map(lambda x: list(map(lambda y: int(y if len(y) > 0 else 0), x.split('\n'))), file.read().split('\n\n')))
+print("Part 1: {}\nPart 2: {}".format(max(map(sum, [m for m in meals])), sum(sorted(map(sum, [m for m in meals]), reverse=True)[:3])))
 
 # First attempt:
-# meals = []
-# with open("../inputs/day1.txt") as file:
-#     for f in file.readlines():
-#         meal = f.strip('\n')
-#         meals.append(int(meal) if len(meal) > 0 else 0)
-#
-# elves = []
-# current_calories = 0
-# for m in meals:
-#     if m > 0:
-#         current_calories += m
-#     else:
-#         elves.append(current_calories)
-#         current_calories = 0
-#
-# print("Part 1: Most calories carried by a single elf =", max(elves))
-#
-# print("Part 2: Total calories counted by top 3 elves =", sum(sorted(elves)[::-1][:3]))
+meals = []
+with open("../inputs/day1.txt") as file:
+    for f in file.readlines():
+        meal = f.strip('\n')
+        meals.append(int(meal) if len(meal) > 0 else 0)
+
+elves = []
+current_calories = 0
+for m in meals:
+    if m > 0:
+        current_calories += m
+    else:
+        elves.append(current_calories)
+        current_calories = 0
+
+print("Part 1: Most calories carried by a single elf =", max(elves))
+
+print("Part 2: Total calories counted by top 3 elves =", sum(sorted(elves)[::-1][:3]))
