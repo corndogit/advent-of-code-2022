394738cf0128f036d530b53e4450441371ec9b05
corndogit <nathanfinlaybrenton@gmail.com>
2022-12-01 14:39:34 +0000

    Add golfed solution for day 1

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,3 +1,11 @@
+# Golfed solution:
+with open("../inputs/day1.txt") as file:
+    meals = list(map(lambda x: list(map(lambda y: int(y if len(y) > 0 else 0), x.split('\n'))), file.read().split('\n\n')))
+    print("Part 1: Most calories carried by a single elf =", max(map(sum, [m for m in meals])))
+    print("Part 2: Total calories counted by top 3 elves =", sum(sorted(map(sum, [m for m in meals]), reverse=True)[:3]))
+
+
+# First attempt:
 meals = []
 with open("../inputs/day1.txt") as file:
     for f in file.readlines():
