546d8eda123a2ca2c5aa8ceaa8394fd21aad26b1
Nathan <101812777+corndogit@users.noreply.github.com>
2022-12-07 19:53:21 +0000

    initial day 6 solution

--- solutions/day6.py
+++ solutions/day6.py
@@ -1,1 +1,10 @@
-datastream = open("../inputs/day6.txt").readline()
+def find_start_marker(datastream, header_size):
+    for chars_processed in range(header_size, len(datastream)):
+        if len(set(datastream[chars_processed-header_size:chars_processed])) == header_size:
+            return chars_processed
+
+
+if __name__ == "__main__":
+    with open("../inputs/day6.txt") as file:
+        input_datastream = file.readline()
+    print(f"Part 1: {find_start_marker(input_datastream, 4)}\nPart 2: {find_start_marker(input_datastream, 14)}")
